11cbddc0e60fef3c385962b71a51d6e3ef43c908
Andrew Jarrett <ajarrett@bestow.com>
2021-10-17 11:50:31 -0500

    actually add qmk stuff

--- bootstrap/qmk.sh
+++ bootstrap/qmk.sh
@@ -1,2 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Installing qmk/qmk/qmk...🐟"
+brew install qmk/qmk/qmk
+qmk setup ahrjarrett/qmk_firmware
+
+printf "\n\e[36mqmk install complete (TODO: flesh out qmk install more)\e[0m\n"
 
 
