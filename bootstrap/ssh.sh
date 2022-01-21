6bde4168bbf4692918fa3d18b90cadd3e3919c02
Andrew Jarrett <andrewjarrett@andrews-mbp-2.lan>
2022-01-21 13:07:28 -0600

    change github link for ssh keygen

--- bootstrap/ssh.sh
+++ bootstrap/ssh.sh
@@ -20,7 +20,7 @@ generate_github_ssh () {
   printf "\nContents of \e[32m$PATH_TO_GH_KEY.pub\e[0m copied to your clipboard \n\n"
   echo "Add public key to GitHub"
   read -p $'\e[36mPress [Enter] key to open GitHub →\e[0m'
-  open https://github.com/account/ssh
+  open https://github.com/settings/ssh/new
   read -p "Press [Enter] key when finished adding key..."
   echo "GitHub key generated! To check your config the link script runs, do:"
   echo "$ ssh -T git@github.com"
